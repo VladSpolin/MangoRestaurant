539948de52878ee2e2493d4d4cb96e942d721e54
spoli <spoli@DESKTOP-KIAE4TV>
2024-12-06 09:21:29 +0300

    Добавьте файлы проекта.

---
+++ Mango.Services.Identity/SD.cs
@@ -0,0 +1,58 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System.Runtime.CompilerServices;
+
+namespace Mango.Services.Identity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("Mango", "Mango Server"),
+                new ApiScope("read", "Read your data."),
+                new ApiScope("write", "Write your data."),
+                new ApiScope("delete", "Delete your data.")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes={"read", "write", "profie"}
+                },
+                new Client
+                {
+                    ClientId = "mango",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "http://localhost:44387/signin-oidc" },
+                    PostLogoutRedirectUris= { "http://localhost:44387/signout-callback-oidc" },
+                    AllowedScopes=new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "mango"
+                  
+                    }
+                },
+            };
+
+    }
+}
---
+++ Mango.Sevices.ProductAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Sevices.ProductAPI.Models;
+using Mango.Sevices.ProductAPI.Models.Dtos;
+
+namespace Mango.Sevices.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductDto,Product>();
+                config.CreateMap<Product, ProductDto>();
+
+            });
+            return mappingConfig;
+        }
+    }
+}
